925cbdf25a2d083b62a95e70c8875bcaf05b78e1
changheeBong <model_so@naver.com>
2024-07-23 14:17:13 +0900

    3주차 미션문제

---
+++ 3rd_hashTable/mission/Pro.java
@@ -0,0 +1,56 @@
+package mission;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Pro {
+
+    public static void main(String[] args) {
+
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        Solution s = new Solution();
+
+        System.out.println(s.solution(clothes));
+    }
+
+
+    static class Solution {
+        public int solution(String[][] clothes) {
+            int answer = 0;
+            Map<String, Integer> map = new HashMap<>();
+            for (String[] clothe : clothes) {
+                if(!map.containsKey(clothe[1])) map.put(clothe[1], 1);
+                else map.put(clothe[1], map.get(clothe[1]) + 1);
+            }
+            int index = 0;
+            int[] arr = new int[4];
+            for (String s : map.keySet()) {
+                arr[index] = map.get(s);
+                index++;
+            }
+            Map<String, String> map = new 
+
+            for(int i= 0; i< arr.length -1; i ++){
+                for(int j = i+1; j < arr.length; j++){
+                    if(arr[j] == 1){
+                        answer += arr[i];
+                    }else {
+                        answer += arr[i] * arr[j];
+                    }
+                    for(int k = arr.length-1; k>j ; k--){
+                        if(arr[k] == 1){
+                         answer += arr[i];
+                        }else {
+                            answer += arr[i] * arr[k];
+                        }
+                    }
+
+                }
+                answer += arr[i];
+            }
+            answer+= arr[3];
+            return answer;
+        }
+    }
+}
