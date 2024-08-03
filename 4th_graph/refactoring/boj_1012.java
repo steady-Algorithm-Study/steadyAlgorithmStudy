ed0dd2ac4a371b0790c7bfd3d4e39e56c18c89d0
SunMi <wywudi@naver.com>
2024-08-03 10:00:00 +0900

    4주차 study

---
+++ 4th_graph/refactoring/boj_1012.java
@@ -0,0 +1,52 @@
+package refactoring;
+
+import java.util.Scanner;
+
+public class boj_1012 {
+    static int p;
+    static int[][] arr;
+    static int[] row ={1, -1, 0, 0};
+    static int[] col = {0, 0, -1, 1};
+
+    public void dfs(int x, int y) {
+        if (arr[x][y] == 1) {
+            arr[x][y] = 0;
+            //상하좌우
+            for (int i = 0; i < row.length; i++) {
+                int rdis = x+row[i];
+                int cdis = y+col[i];
+                //0보다 크고, 범위를 벗어나지 않은
+                if(rdis>=0&&rdis<arr.length&&cdis>=0&&cdis<arr[rdis].length) {
+                    dfs(rdis, cdis);
+                }
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            int p = sc.nextInt();
+            int q=sc.nextInt();
+            int k = sc.nextInt();
+            arr=new int[p][q];
+            //배추 심기
+            for(int j=0;j<k;j++) {
+                arr[sc.nextInt()][sc.nextInt()]=1;
+            }
+            int answer = 0;
+            //배추 탐색
+            for (int x = 0; x < arr.length; x++) {
+                for (int y = 0; y < arr[i].length; y++) {
+                    if(arr[x][y]==1){
+                        answer++;
+                        new boj_1012().dfs(x,y);
+                    }
+                }
+            }
+            System.out.println(answer);
+        }
+    }
+}
---
+++ 4th_graph/refactoring/boj_1325.java
@@ -0,0 +1,61 @@
+package refactoring;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class boj_1325 {
+    static int n;
+    static ArrayList<ArrayList<Integer>> list;
+    static int[] count;
+    static int max = 0;
+
+    public static int bfs(int start) {
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = true;
+        int L = 0;
+
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            L++;
+            for (int num : list.get(x)) {
+                if (!visited[num]) {
+                    visited[num] = true;
+                    q.add(num);
+                }
+            }
+        }
+        return L;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int m = sc.nextInt();
+        list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            list.get(y).add(x);
+        }
+
+        count = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            count[i] = bfs(i);
+            max = Math.max(max, count[i]);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (count[i] == max) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
