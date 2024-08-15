738d69b78b8ea415442b462994ad1b8468fd6714
SunMi <wywudi@naver.com>
2024-08-15 15:13:33 +0900

    2주차 study

---
+++ 2nd_dequePriorityQueue/refactoring/n13904.java
@@ -0,0 +1,59 @@
+package refactoring;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class n13904 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PriorityQueue<Subject> pq = new PriorityQueue<>();
+        //nlogn+n
+        for (int i = 0; i < n; i++) {
+            pq.offer(new Subject(sc.nextInt(), sc.nextInt()));
+        }
+
+        int size=pq.stream().reduce((x,y)->x.getD()>y.getD()?x:y).get().getD();
+        //가장 마지막 마감일 d만큼의 크기만큼 배열 할당
+        int[] arr=new int[size];
+        int sum = 0;
+
+        while(!pq.isEmpty()) {
+            int num = pq.peek().getW();
+            int day = pq.peek().getD();
+                //마감일까지 과제를 해결할 수 있는 날이 있다면
+                for (int j = day-1; j >= 0; j--) {
+                    if(arr[j]==0) {
+                        sum += num;
+                        arr[j] = 1;
+                        break;
+                    }
+                }
+            pq.poll();
+        }
+        System.out.println(sum);
+
+    }
+}
+
+class Subject implements Comparable<Subject> {
+    int d, w;
+
+    public Subject(int d, int w) {
+        this.d = d;
+        this.w = w;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    @Override
+    public int compareTo(Subject s) {
+        return s.getW()-this.w;
+    }
+}
---
+++ 2nd_dequePriorityQueue/refactoring/n29160.java
@@ -0,0 +1,50 @@
+package refactoring;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class n29160 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<Integer, PriorityQueue<Integer>>(11);
+
+        for(int i = 1; i <= n; i++){
+            //최대힙으로
+            map.put(i, new PriorityQueue<Integer>(Collections.reverseOrder()));
+        }
+        //선수를 포지션에 맞는 위치로 할당
+        for (int i = 1; i <= n; i++) {
+            int p=sc.nextInt();
+            int w =sc.nextInt();
+            map.get(p).add(w);
+        }
+
+
+        //k년
+        for(int j = 1; j <= k; j++) {
+            //선발 선수 선택 후 -1
+            for (int i = 1; i <= n; i++) {
+                if (!map.get(i).isEmpty()) {
+                    PriorityQueue<Integer> pq = map.get(i);
+                   //선수의 점수가 0보다 큼
+                    if (pq.poll() - 1 < 0) {
+                        pq.offer(0);
+                    }else pq.offer(pq.poll() - 1);
+                }
+            }
+        }
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++){
+            sum += map.get(i).poll();
+        }
+        System.out.println(sum);
+
+
+    }
+}
